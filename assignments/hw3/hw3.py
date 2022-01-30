305e5b6ff269ec2452a4604f0b89d9402e93f69b
kratt010 <krattbs@g.cofc.edu>
2022-01-30 15:16:17 -0500

    Complete functional first draft of hw3.py

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,55 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Brendan Kratt
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Brief description of the problem that this program solves>
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    num_grades = eval(input("how many grades will you enter? "))
+    grades_lst = []
+    for i in range(num_grades):
+        grades_lst.append(eval(input("Enter grade: ")))
+    print("average is", (sum(grades_lst) / len(grades_lst)))
 
 
 def tip_jar():
-    pass
+    tip_bal = []
+    for i in range(5):
+        tip_bal.append(eval(input("how much would you like to donate? ")))
+    print("total tips:", sum(tip_bal))
 
 
 def newton():
-    pass
+    radicand = eval(input("What number do you want to square root? "))
+    iterations = eval(input("How many times should we improve the approximation? "))
+    approx = radicand
+    for i in range(iterations):
+        approx = (approx+radicand/approx)/2
+    print("the square root is approximately", approx)
 
 
 def sequence():
-    pass
+    num_terms = eval(input("how many terms would you like? "))
+    for i in range(1, num_terms+1):
+        print(i - (1 - i % 2))  # If i even, print i minus 1. Odd, print i.
 
 
 def pi():
-    pass
+    list_terms = []
+    num_terms = eval(input("how many terms in the series? "))
+    for i in range(2, num_terms+2):
+        denom_value = i - (1 - i % 2)  # if i even, denom_values equals i minus 1. Else, equals i.
+        num_value = i - (i % 2)  # If i odd, num_value equals i minus 1. Else, equals i.
+        list_terms.append(num_value/denom_value)
+    pi_value = 1
+    for term in list_terms:
+        pi_value *= term
+    print(pi_value * 2)
 
 
 if __name__ == '__main__':
