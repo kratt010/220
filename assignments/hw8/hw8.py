b0595b4909743b70d0ee3a16bb914aa2c480f1df
kratt010 <krattbs@g.cofc.edu>
2022-03-13 19:43:00 -0400

    push complete hw8

--- assignments/hw8/hw8.py
+++ assignments/hw8/hw8.py
@@ -1,45 +1,94 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Brendan Kratt
+hw8.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This program defines various functions dealing with return statements and conditionals.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+import math
+from graphics import GraphWin, Text, Circle, Point
 
 
 def add_ten(nums):
-    pass
+    lst = []
+    for i in nums:
+        lst.append(i+10)
+    nums.clear()
+    for i in lst:
+        nums.append(i)
 
 
 def square_each(nums):
-    pass
+    lst = []
+    for i in nums:
+        lst.append(i * i)
+    nums.clear()
+    for i in lst:
+        nums.append(i)
 
 
 def sum_list(nums):
-    pass
+    return sum(nums)
 
 
 def to_numbers(nums):
-    pass
+    nums_calculable = []
+    for i in nums:
+        nums_calculable.append(eval(i))
+    nums.clear()
+    for i in nums_calculable:
+        nums.append(i)
 
 
 def sum_of_squares(nums):
-    pass
+    output_lst = []
+    for i in nums:
+        iter_var = i.split(", ")
+        to_numbers(iter_var)
+        square_each(iter_var)
+        iter_var = sum_list(iter_var)
+        output_lst.append(iter_var)
+    return output_lst
 
 
 def starter(weight, wins):
-    pass
+    if wins > 20:
+        return True
+    if weight > 199:
+        return True
+    if weight >= 150:
+        if weight < 160:
+            if wins >= 5:
+                return True
+    return False
 
 
 def leap_year(year):
-    pass
+    if year % 100 == 0:
+        return bool(year % 400 == 0)
+    if year % 4 == 0:
+        return True
+    return False
 
 
-def circle_overlap():
+def did_overlap(circle_one, circle_two):
+    center_1 = circle_one.getCenter()
+    center_2 = circle_two.getCenter()
+    x_component = (center_2.getX() - center_1.getX()) ** 2
+    y_component = (center_2.getY() - center_1.getY()) ** 2
+    distance_btwn_centers = math.sqrt(x_component + y_component)
+
+    sum_radii = circle_one.getRadius() + circle_two.getRadius()
+
+    if distance_btwn_centers > sum_radii:
+        return False
+    return True
+
+
+def circle_overlap():  # "Too many locals," but can only comply with coding norms with that many.
+    # defines window
     width_px = 700
     height_px = 700
     win = GraphWin("Circle", width_px, height_px)
@@ -47,19 +96,41 @@ def circle_overlap():
     height = 10
     win.setCoords(0, 0, width, height)
 
-    center = win.getMouse()
-    circumference_point = win.getMouse()
-    radius = math.sqrt(
-        (center.getX() - circumference_point.getX()) ** 2 + (center.getY() - circumference_point.getY()) ** 2)
-    circle_one = Circle(center, radius)
-    circle_one.setFill("light blue")
-    circle_one.draw(win)
+    # defines, draws circle1
+    center_1 = win.getMouse()
+    circumference_point_1 = win.getMouse()
+    radius_1 = math.sqrt(  # This line is too long, but I wasn't the one who wrote it.
+        (center_1.getX() - circumference_point_1.getX()) ** 2 + (center_1.getY() - circumference_point_1.getY()) ** 2)
+    circle_1 = Circle(center_1, radius_1)
+    circle_1.setFill("light blue")
+    circle_1.draw(win)
 
+    # defines, draws circle2
+    center_2 = win.getMouse()
+    circumference_point_2 = win.getMouse()
+    radius_2 = math.sqrt(                   # ^^ See above
+        (center_2.getX() - circumference_point_2.getX()) ** 2 + (center_2.getY() - circumference_point_2.getY()) ** 2)
+    circle_2 = Circle(center_2, radius_2)
+    circle_2.setFill("light green")
+    circle_2.draw(win)
+
+    # defines, draws determ textbox
+
+    bool_val = did_overlap(circle_1, circle_2)
+    if bool_val is False:
+        determ_msg = "The circles do not overlap"
+    else:
+        determ_msg = "The circles overlap"
+
+    determ_txt_pt = Point(width/2, height/3)
+    determ_txt = Text(determ_txt_pt, determ_msg)
+    determ_txt.draw(win)
+
+    # defines, draws closing message
+    close_pt = Point(width/2, height/4)
+    close_txt = Text(close_pt, "Click again to close")
+    close_txt.draw(win)
     win.getMouse()
-
-
-def did_overlap(circle_one, circle_two):
-    pass
 
 
 if __name__ == '__main__':
