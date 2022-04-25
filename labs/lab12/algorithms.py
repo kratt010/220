3140227590749b39c5fc0f41a838401aa84e4800
kratt010 <97077826+kratt010@users.noreply.github.com>
2022-04-25 15:33:29 -0400

    push complete lab13, trade_alert file

--- labs/lab12/algorithms.py
+++ labs/lab12/algorithms.py
@@ -69,3 +69,59 @@ def hi_lo_game():
         print("You win in", tries, "guesses!")
     if won is False:
         print("Sorry, you lose. The number was " + str(random_num) + ".")
+
+# lab 13
+
+
+def is_in_binary(search_val, values):
+    # binary search assumed a sorted list, so we utilize that.
+    low_pos = 0
+    high_pos = len(values) - 1
+    while low_pos <= high_pos:
+        middle = (low_pos + high_pos) // 2
+        if search_val == values[middle]:
+            return True
+        elif search_val > values[middle]:
+            low_pos = middle + 1
+        else:
+            high_pos = middle - 1
+    return False
+
+
+def selection_sort(values):
+    len_lst = len(values)
+    for i in range(len_lst):
+        lowest_pos = i  # assumes current position is position of lowest val
+        for g in range(i + 1, len_lst):
+            if values[lowest_pos] > values[g]:  # if assumption invalidated, change value to new pos
+                lowest_pos = g
+
+        # if value changed from assumption, swap positions.
+        # else, do nothing.
+        if not lowest_pos == i:
+            placeholder = values[i]
+            values[i] = values[lowest_pos]
+            values[lowest_pos] = placeholder
+    return values
+
+
+def calc_area(rect):
+    # abs. value lets us sidestep the failure case that arises if p1 and p2 are out of order.
+    length = abs(rect.getP1().getX() - rect.getP2().getX())
+    height = abs(rect.getP1().getY() - rect.getP2().getY())
+    area = length * height
+    return area
+
+
+def rect_sort(rectangles):
+    lst_areas = []
+    for rect in rectangles:
+        area = calc_area(rect)
+        lst_areas.append(area)  # pos of rect on original list == pos of area on new list
+    # defines dictionary. key == area, value == rectangle
+    my_dict = dict(zip(lst_areas, rectangles))
+    sorted_areas = selection_sort(lst_areas)
+    sorted_rectangles = []
+    for value in sorted_areas:
+        sorted_rectangles.append(my_dict[value])
+    return sorted_rectangles
